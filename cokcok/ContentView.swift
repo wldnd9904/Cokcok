0cd7a090ac8bf12a48904366ef0d12c56e06338f
wldnd9904 <wldnd9904@uos.ac.kr>
2023-11-08 13:56:09 +0900

    feat: 하는김에 메인화면 네비게이션스택 추가

--- cokcok/ContentView.swift
+++ cokcok/ContentView.swift
@@ -10,22 +10,31 @@ import SwiftUI
 struct ContentView : View {
     var body: some View {
         TabView {
-            Summary()
-                .tabItem {
-                    Image(systemName: "list.bullet.clipboard")
-                    Text("요약")
-                }
-            Matches()
-                .tabItem {
-                    Image(systemName: "waveform.path.ecg.rectangle")
-                    Text("경기 분석")
-                }
-                .badge(10)
-            SwingView()
-                .tabItem {
-                    Image(systemName: "figure.badminton")
-                    Text("스윙 분석")
-                }
+            NavigationStack{
+                Summary()
+                    .navigationTitle("요약")
+            }
+            .tabItem {
+                Image(systemName: "list.bullet.clipboard")
+                Text("요약")
+            }
+            NavigationStack {
+                Matches()
+                    .navigationTitle("경기 분석")
+            }
+            .tabItem {
+                Image(systemName: "waveform.path.ecg.rectangle")
+                Text("경기 분석")
+            }
+            .badge(10)
+            NavigationStack{
+                SwingView()
+                    .navigationTitle("스윙 분석")
+            }
+            .tabItem {
+                Image(systemName: "figure.badminton")
+                Text("스윙 분석")
+            }
         }
         .background(.thinMaterial)
         .font(.headline)
