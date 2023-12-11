7ab79681ef74ff7856dffa7a787d019edea81aa8
wldnd9904 <wldnd9904@uos.ac.kr>
2023-12-11 20:10:01 +0900

    feat: 분류되지 않은 스윙은 그래프에서 x로 표시, 선 안그림

--- cokcok/Views/Matches/StrokeChart.swift
+++ cokcok/Views/Matches/StrokeChart.swift
@@ -14,33 +14,48 @@ struct StrokeChart: View {
     @Binding var selectedSwing: SwingType?
     var body: some View {
         Chart{
-            ForEach(swings, id: \.id) { swing in 
-                LineMark(x:.value("index",swing.id),
-                    y:.value("score",swing.score))
+            ForEach(swings, id: \.id) { swing in
+                if(swing.type == .x) {
+                    PointMark(x:.value("index",swing.id),
+                              y:.value("score",swing.score))
+                    .symbol{
+                        Image(systemName: "xmark")
+                            .foregroundStyle(.red)
+                            .bold()
+                    }
+                }
+            }
+            ForEach(swings, id: \.id) { swing in
+                if(swing.type != .x) {
+                    LineMark(x:.value("index",swing.id),
+                             y:.value("score",swing.score))
+                    .symbol(.circle)
+                }
             }
             .interpolationMethod(.catmullRom)
-            .symbol(.circle)
             .symbolSize(50)
             .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.gray]),startPoint: .top,
                                       endPoint: .bottom))
             
             ForEach(swings, id: \.id) { swing in
-                PointMark(x: .value("index",swing.id), y:.value("score", swing.score))
-                    .annotation(position: .top){
-                        Text("\(swing.score)")
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                            .opacity(swing.type == selectedSwing ? 1.0 : 0.0)
-                            .animation(.easeInOut, value: selectedSwing)
-                    }
-                    .symbol {
+                if(swing.type != .x){
+                    PointMark(x: .value("index",swing.id), y:.value("score", swing.score))
+                        .annotation(position: .top){
+                            Text("\(swing.score)")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                                .opacity(swing.type == selectedSwing ? 1.0 : 0.0)
+                                .animation(.easeInOut, value: selectedSwing)
+                        }
+                        .symbol {
                             Circle()
-                            .stroke(swing.type.color, lineWidth:3)
+                                .stroke(swing.type.color, lineWidth:3)
                                 .foregroundStyle(swing.type.color)
                                 .scaleEffect(swing.type == selectedSwing ? 1.2 : 0.6)
                                 .animation(.easeInOut, value: selectedSwing)
                                 .frame(width:10)
-                    }
+                        }
+                }
             }
         }
         .chartXScale(domain:1...swings.count+1)
