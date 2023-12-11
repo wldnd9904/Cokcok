edb5e967c798121fda6589eb8c09b5fd10380db5
wldnd9904 <wldnd9904@uos.ac.kr>
2023-12-11 13:02:52 +0900

    feat: 스윙 도움말 영상 예시 추가

--- cokcok/Views/Swing/SwingRecordView.swift
+++ cokcok/Views/Swing/SwingRecordView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import HealthKit
+import Giffy
 
 struct SwingRecordView: View {
     @EnvironmentObject var modelData:ModelData
@@ -16,104 +17,112 @@ struct SwingRecordView: View {
     @State var errorMsg:String? = nil
     let dismiss: () -> Void
     var body: some View {
-        if(errorMsg != nil){
-            VStack{
-                Text(errorMsg!)
-                Button("닫기"){dismiss()}
-            }
-        }else if(swingResult != nil){
-            SwingDetail(swing: swingResult!)
-        } else if(model.state == .saved || model.state == .sending || model.state == .sent){
-            SwingResultView(folderName: model.folderName, state: $model.state) {
-                model.uploadSwing(token:modelData.user!.id){ swingAnalyze in
-                    DispatchQueue.main.async{
-                        modelData.swings.append(swingAnalyze)
-                    }
-                    dismiss()
-                }onError:{
-                    errorMsg = $0
+        GeometryReader{ geo in
+            if(errorMsg != nil){
+                VStack{
+                    Text(errorMsg!)
+                    Button("닫기"){dismiss()}
                 }
-            } onCancel: {
-                model.resetRecording()
-            }
-        } else {
-            ZStack {
-                ProgressView()
-                model.preview
-                    .ignoresSafeArea()
-                 
-                VStack {
-                    Spacer()
-                    HStack(spacing:20){
-                        Button(action: {
-                            model.state == .running ? model.stopRecording(): model.startRecording()
-                        }){
-                            Image(systemName: model.state == .running ? "stop.circle" : "record.circle")
+            }else if(swingResult != nil){
+                SwingDetail(swing: swingResult!)
+            } else if(model.state == .saved || model.state == .sending || model.state == .sent){
+                SwingResultView(folderName: model.folderName, state: $model.state) {
+                    model.uploadSwing(token:modelData.user!.id){ swingAnalyze in
+                        DispatchQueue.main.async{
+                            modelData.swings.append(swingAnalyze)
                         }
-                        .disabled(!model.isReachable ||
-                                  model.state.isError())
-                        Button(action: {
-                            model.switchCameraInput()
-                        }) {
-                            Image(systemName: "arrow.triangle.2.circlepath.camera")
-                        }
-                        
-                        Button(action: {
-                            showHelp = true
-                        }) {
-                            Image(systemName: "questionmark.circle")
-                        }
-                        .foregroundColor(model.isButtonActivated ? .yellow : .gray)
-                        
-                        Button(action: {
-                            dismiss()
-                        }) {
-                            Image(systemName: "xmark.circle")
-                        }
-                        .foregroundColor(model.isButtonActivated ? .red : .gray)
+                        dismiss()
+                    }onError:{
+                        errorMsg = $0
                     }
-                    .disabled(!model.isButtonActivated && !model.state.isError())
-                    .padding(10)
-                    .font(.title)
-                    .background(.white)
-                    .cornerRadius(30)
-                    .shadow(radius: 3)
+                } onCancel: {
+                    model.resetRecording()
                 }
-                
-                VStack(alignment: .center) {
-                    if(!model.isReachable) {
-                        HStack{
-                            Spacer()
-                            VStack{
-                                if !model.isReachable {
-                                    Text("애플워치가 연결되지 않았습니다.")
-                                    Button(action: {
-                                        model.state = .idle
-                                        model.startCokcokWatchApp()
-                                    }) {
-                                        Text("애플워치에서 콕콕 열기")
+            } else {
+                ZStack {
+                    ProgressView()
+                    model.preview
+                        .ignoresSafeArea()
+                    if(showHelp){
+                        Color.black.ignoresSafeArea()
+                        Giffy("sample")
+                            .scaledToFill()
+                            .frame(width:geo.size.width, height:geo.size.height)
+                            .clipped()
+                    }
+                    VStack {
+                        Spacer()
+                        HStack(spacing:20){
+                            Button(action: {
+                                model.state == .running ? model.stopRecording(): model.startRecording()
+                            }){
+                                Image(systemName: model.state == .running ? "stop.circle" : "record.circle")
+                            }
+                            .disabled(!model.isReachable ||
+                                      model.state.isError())
+                            Button(action: {
+                                model.switchCameraInput()
+                            }) {
+                                Image(systemName: "arrow.triangle.2.circlepath.camera")
+                            }
+                            
+                            Button(action: {
+                                showHelp = true
+                            }) {
+                                Image(systemName: "questionmark.circle")
+                            }
+                            .foregroundColor(model.isButtonActivated ? .yellow : .gray)
+                            
+                            Button(action: {
+                                dismiss()
+                            }) {
+                                Image(systemName: "xmark.circle")
+                            }
+                            .foregroundColor(model.isButtonActivated ? .red : .gray)
+                        }
+                        .disabled(!model.isButtonActivated && !model.state.isError())
+                        .padding(10)
+                        .font(.title)
+                        .background(.white)
+                        .cornerRadius(30)
+                        .shadow(radius: 3)
+                    }
+                    
+                    VStack(alignment: .center) {
+                        if(!model.isReachable) {
+                            HStack{
+                                Spacer()
+                                VStack{
+                                    if !model.isReachable {
+                                        Text("애플워치가 연결되지 않았습니다.")
+                                        Button(action: {
+                                            model.state = .idle
+                                            model.startCokcokWatchApp()
+                                        }) {
+                                            Text("애플워치에서 콕콕 열기")
+                                        }
                                     }
                                 }
+                                .padding(.bottom, 7)
+                                Spacer()
                             }
-                            .padding(.bottom, 7)
-                            Spacer()
+                            .background(Color(red: 238/255, green: 126/255, blue: 115/255))
                         }
-                        .background(Color(red: 238/255, green: 126/255, blue: 115/255))
-                    }
-                    if(model.state != .idle) {
-                        HStack{
-                            Spacer()
-                            Text(model.state.message)
-                                .padding(3)
-                            Spacer()
+                        if(model.state != .idle) {
+                            HStack{
+                                Spacer()
+                                Text(model.state.message)
+                                    .padding(3)
+                                Spacer()
+                            }
+                            .background(Color(red:249/255, green: 221/255, blue:102/255))
                         }
-                        .background(Color(red:249/255, green: 221/255, blue:102/255))
+                        Spacer()
                     }
-                    Spacer()
+                    .animation(.easeIn, value: model.state)
+                    
+                    if(showHelp) {SwingHelpView(onClose: {showHelp = false})}
                 }
-                .animation(.easeIn, value: model.state)
-                
-                if(showHelp) {SwingHelpView(onClose: {showHelp = false})}
             }
         }
     }
